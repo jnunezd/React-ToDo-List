c86761eb1bb829fab07318827b7c49e06e5f020a
Joaquin Nuñez <joaquin16602@gmail.com>
2019-12-13 23:38:56 +0000

    Simple to-do list with react

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 export function Home() {
+	const [message, setMessage] = useState([
+		"Cras justo odio",
+		"Dapibus ac facilisis in",
+		"Morbi leo risus",
+		"Porta ac consectetur ac",
+		"Vestibulum at eros"
+	]);
+
+	const [newMessage, setNewMessage] = useState("");
+
+	function addMessage(e) {
+		if (e.target.value != "" && e.keyCode == 13) {
+			setMessage(message.concat(e.target.value));
+			e.target.value = "";
+		}
+	}
+
+	function deleteMessage(i) {
+		let mess = message.slice();
+		mess.splice(i, 1);
+		setMessage(mess);
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="card">
+			<div className="card-body">
+				<div className="form-group">
+					<input
+						type="text"
+						name=""
+						className="form-control"
+						placeholder="What needs to be done?"
+						onKeyDown={e => addMessage(e)}
+					/>
+				</div>
+				<ul className="list-group">
+					{message.length > 0 &&
+						message.map((item, i) => {
+							return (
+								<li key={i} class="list-group-item">
+									{item}{" "}
+									<i
+										className="fa fa-trash float-right"
+										onClick={() => deleteMessage(i)}
+									/>
+								</li>
+							);
+						})}
+				</ul>
+				<p className="card-text">
+					{message.length > 0
+						? message.length + " items left."
+						: "You don't have To-do's"}
+				</p>
+			</div>
 		</div>
 	);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,34 @@ import "bootstrap";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
 import { Home } from "./component/home.js";
 
-//render your react application
+//Titulo
+function Title() {
+	return (
+		<div className="titulo">
+			<h1>To-Do List</h1>
+		</div>
+	);
+}
+
+//bloque de por hacer
+function Todo(props) {
+	return (
+		<div>
+			<h3 className="todo">{props.text}</h3>
+			<button className="btn btn-primary">
+				<i className="far fa-times-circle" />
+			</button>
+		</div>
+	);
+}
+
+//Contador
+function Counter(props) {
+	return <p>{props.count}</p>;
+}
+
+// Suponiendo que haya un div con la id 'app' en el cuerpo de su sitio web original
+// toda tu aplicación de react se agregará a esa div
 ReactDOM.render(<Home />, document.querySelector("#app"));
